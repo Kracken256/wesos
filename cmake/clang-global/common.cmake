dc2fca238344456385df5b2649720a13c649aece
Wesley Jones <wesjones2004@gmail.com>
2025-04-27 22:48:58 -0400

    setup default config

---
+++ cmake/clang-global/common.cmake
@@ -0,0 +1,25 @@
+cmake_minimum_required(VERSION 3.15)
+
+################################################################################
+# LINTING AND REPORTING
+################################################################################
+add_compile_options(-Wall -Wextra -Wpedantic -Werror)
+add_compile_options(-Wshadow -Wconversion -Wsign-conversion)
+add_compile_options(-Wno-error=unused-parameter -Wno-error=unused-variable -Wno-error=unused-function)
+add_compile_options(-Wno-error=deprecated)
+
+################################################################################
+# SECURITY
+################################################################################
+add_compile_options(-fstack-protector) # Ensure stack canaries are present
+add_compile_options(-fPIC) # Position Independent Code for ASLR and stuff
+
+################################################################################
+# OTHER
+################################################################################
+add_compile_options(-fno-omit-frame-pointer) # Concepts like "backtrace()" require this.
+add_compile_options(-fno-rtti -fno-exceptions) # Because I want it to work.
+
+set(CMAKE_CXX_STANDARD 20)
+set(CMAKE_CXX_STANDARD_REQUIRED ON)
+set(CMAKE_CXX_EXTENSIONS OFF)
