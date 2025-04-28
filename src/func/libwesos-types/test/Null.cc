c3a8f3b1a69b5d98d38ac41de766ca856a30c126
Wesley Jones <wesjones2004@gmail.com>
2025-04-28 14:48:41 -0400

    wesos-types: added some unit tests

---
+++ src/func/libwesos-types/test/Null.cc
@@ -0,0 +1,28 @@
+/**
+ * This file is part of the WesOS project.
+ *
+ * WesOS is public domain software: you can redistribute it and/or modify
+ * it under the terms of the Unlicense(https://unlicense.org/).
+ */
+
+#include <gtest/gtest.h>
+
+#include <wesos-types/Types.hh>
+
+TEST(Null, Null) {
+  // Ensure proper type alias
+  static_assert(wesos::types::is_same_v<decltype(wesos::types::null), const wesos::types::Null>);
+
+  {  // Check instance identity properties
+    const auto &obj = wesos::types::null;
+
+    EXPECT_LE(sizeof(obj), 1);
+    EXPECT_EQ(wesos::types::Null(), obj);
+    EXPECT_FALSE(obj < obj);
+    EXPECT_FALSE(obj > obj);
+    EXPECT_TRUE(obj <= obj);
+    EXPECT_TRUE(obj >= obj);
+    EXPECT_EQ(obj, obj);
+    EXPECT_FALSE(obj != obj);
+  }
+}
