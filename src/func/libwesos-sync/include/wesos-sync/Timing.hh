ec47542e78bc15036be825abd460be4446e0fb00
Wesley Jones <wesjones2004@gmail.com>
2025-04-29 01:30:47 -0400

    wesos-sync: support ephemeral_pause()

---
+++ src/func/libwesos-sync/include/wesos-sync/Timing.hh
@@ -0,0 +1,12 @@
+/**
+ * This file is part of the WesOS project.
+ *
+ * WesOS is public domain software: you can redistribute it and/or modify
+ * it under the terms of the Unlicense(https://unlicense.org/).
+ */
+
+#pragma once
+
+namespace wesos::sync {
+  void ephemeral_pause();
+}  // namespace wesos::sync
---
+++ src/func/libwesos-sync/src/Timing.cc
@@ -0,0 +1,17 @@
+/**
+ * This file is part of the WesOS project.
+ *
+ * WesOS is public domain software: you can redistribute it and/or modify
+ * it under the terms of the Unlicense(https://unlicense.org/).
+ */
+
+#include <wesos-assert/Export.hh>
+#include <wesos-sync/Timing.hh>
+
+SYM_EXPORT void wesos::sync::ephemeral_pause() {
+#if defined(__x86_64__)
+  asm volatile("pause" ::: "memory");
+#else
+#error "This implementation of ephemeral_pause() does not support your architecure. Sorry.."
+#endif
+}
