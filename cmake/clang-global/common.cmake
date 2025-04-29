5adb66a4c5de186f1a6340cdf05af4b7baf0e872
Wesley Jones <wesjones2004@gmail.com>
2025-04-28 21:33:41 -0400

    Disable strict aliasing

--- cmake/clang-global/common.cmake
+++ cmake/clang-global/common.cmake
@@ -18,7 +18,7 @@ add_compile_options(-fPIC) # Position Independent Code for ASLR and stuff
 # OTHER
 ################################################################################
 add_compile_options(-fno-omit-frame-pointer) # Concepts like "backtrace()" require this.
-add_compile_options(-fno-rtti -fno-exceptions) # Because I want it to work.
+add_compile_options(-fno-rtti -fno-exceptions -fno-strict-aliasing) # Because I want it to work.
 add_compile_options(-nostdlib -fno-asynchronous-unwind-tables -fno-unwind-tables)
 add_compile_options(-Wno-unused-command-line-argument)
 add_compile_options(-fvisibility=hidden -fvisibility-inlines-hidden)
