f0ce0fb9c71010724a832c770cb0df892c5d7002
Wesley Jones <wesjones2004@gmail.com>
2025-04-29 01:32:12 -0400

    wesos-sync: idk how to test it

---
+++ src/func/libwesos-sync/test/SpinLock.cc
@@ -0,0 +1,14 @@
+/**
+ * This file is part of the WesOS project.
+ *
+ * WesOS is public domain software: you can redistribute it and/or modify
+ * it under the terms of the Unlicense(https://unlicense.org/).
+ */
+
+#include <gtest/gtest.h>
+
+#include <wesos-sync/Atomic.hh>
+
+/// TODO: How the fuck am I going to test a spin lock?
+
+TEST(Something, Idk) {}
