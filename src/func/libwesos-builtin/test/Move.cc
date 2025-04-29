ef24b745203b71b3354a783afcce05f286961c64
Wesley Jones <wesjones2004@gmail.com>
2025-04-29 00:30:38 -0400

    wesos-builtin: unit test move semantics

--- src/func/libwesos-builtin/test/Move.cc
+++ src/func/libwesos-builtin/test/Move.cc
@@ -10,5 +10,44 @@
 #include <wesos-builtin/Move.hh>
 
 TEST(Move, Move) {
-  /// TODO:
+  class MoveSemanticsTest {
+    bool m_active = true;
+    volatile unsigned &m_counter;
+
+  public:
+    MoveSemanticsTest(volatile unsigned &counter) : m_counter(counter) {}
+    MoveSemanticsTest(MoveSemanticsTest &&other) : m_counter(other.m_counter) {
+      other.m_active = false;
+    }
+    MoveSemanticsTest(const MoveSemanticsTest &) = default;
+    ~MoveSemanticsTest() {
+      if (m_active) {
+        m_counter = m_counter + 1;
+      }
+    }
+  };
+
+  volatile unsigned counter = 0;
+
+  {  // Test move semantics
+    {
+      auto obj = MoveSemanticsTest(counter);
+      auto obj2 = wesos::move(obj);
+      (void)obj2;
+    }
+
+    EXPECT_EQ(counter, 1);
+  }
+
+  counter = 0;
+
+  {  // Test copy semantics
+    {
+      auto obj = MoveSemanticsTest(counter);
+      auto obj2 = obj;
+      (void)obj2;
+    }
+
+    EXPECT_EQ(counter, 2);
+  }
 }
