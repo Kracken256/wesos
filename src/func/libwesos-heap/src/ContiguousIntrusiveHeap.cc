82073b93a7ff7d4fdfeafa4de073a16a194b5f06
Wesley Jones <wesjones2004@gmail.com>
2025-04-28 20:14:06 -0400

    wesos-heap: scaffold bare metal heap library

---
+++ src/func/libwesos-heap/src/ContiguousIntrusiveHeap.cc
@@ -0,0 +1,8 @@
+/**
+ * This file is part of the WesOS project.
+ *
+ * WesOS is public domain software: you can redistribute it and/or modify
+ * it under the terms of the Unlicense(https://unlicense.org/).
+ */
+
+#include <wesos-heap/ContiguousIntrusiveHeap.hh>
