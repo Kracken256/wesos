ab81f275da22b7b2cd8c8282f6550b85ad7d8d5a
Wesley Jones <wesjones2004@gmail.com>
2025-04-28 21:34:50 -0400

    wesos-heap: work

--- src/func/libwesos-heap/include/wesos-heap/ContiguousIntrusiveHeap.hh
+++ src/func/libwesos-heap/include/wesos-heap/ContiguousIntrusiveHeap.hh
@@ -7,6 +7,26 @@
 
 #pragma once
 
+#include <wesos-heap/HeapProtocol.hh>
+#include <wesos-types/Types.hh>
+
 namespace wesos::heap {
-  /// TODO:
+  class ContiguousIntrusiveHeap : public HeapProtocol {
+    View<u8> m_bank;
+
+  protected:
+    [[nodiscard, gnu::pure]] auto virt_allocate(usize n_bytes,
+                                                usize align) -> NullableOwnPtr<u8> override;
+
+    void virt_deallocate(NullableOwnPtr<u8> ptr) override;
+
+  public:
+    constexpr ContiguousIntrusiveHeap(View<u8> mem) : m_bank(mem) {}
+    constexpr ContiguousIntrusiveHeap(const ContiguousIntrusiveHeap&) = delete;
+    constexpr ContiguousIntrusiveHeap(ContiguousIntrusiveHeap&&) = delete;
+    constexpr auto operator=(const ContiguousIntrusiveHeap&) -> ContiguousIntrusiveHeap& = delete;
+    constexpr auto operator=(ContiguousIntrusiveHeap&&) -> ContiguousIntrusiveHeap& = delete;
+    constexpr auto operator<=>(const ContiguousIntrusiveHeap&) const = delete;
+    ~ContiguousIntrusiveHeap() override = default;
+  };
 }  // namespace wesos::heap
--- src/func/libwesos-heap/src/ContiguousIntrusiveHeap.cc
+++ src/func/libwesos-heap/src/ContiguousIntrusiveHeap.cc
@@ -5,4 +5,17 @@
  * it under the terms of the Unlicense(https://unlicense.org/).
  */
 
+#include <wesos-assert/Export.hh>
 #include <wesos-heap/ContiguousIntrusiveHeap.hh>
+
+using namespace wesos::heap;
+
+SYM_EXPORT auto ContiguousIntrusiveHeap::virt_allocate(usize n_bytes,
+                                                       usize align) -> NullableOwnPtr<u8> {
+  /// TODO: Implement heap allocate
+  return null;
+}
+
+SYM_EXPORT void ContiguousIntrusiveHeap::virt_deallocate(NullableOwnPtr<u8> ptr) {
+  /// TODO: Implement heap deallocate
+}
