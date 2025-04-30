62d8b1df70e1432ceb456c8334660b176c79e33a
Wesley Jones <wesjones2004@gmail.com>
2025-04-30 14:32:13 -0400

    wesos-heap: Benchmark IntrusivePool

--- src/func/libwesos-heap/bench/IntrusivePool.cc
+++ src/func/libwesos-heap/bench/IntrusivePool.cc
@@ -7,6 +7,7 @@
 
 #include <benchmark/benchmark.h>
 
+#include <iostream>
 #include <wesos-heap/IntrusivePool.hh>
 
 #include "UnifiedBenchmark.hh"
@@ -15,43 +16,71 @@ using namespace wesos;
 using namespace wesos::heap;
 using namespace wesos::heap::testing;
 
-static void BM_IntrusivePool_Creation(benchmark::State& state) {
-  usize object_size = 0;
-  usize object_align = 1;
+using ClampedAlign = ClampMost<PowerOfTwo<usize>, 4096ULL>;
 
-  for (auto _ : state) {
-    auto mm = IntrusivePool(object_size, object_align);
+static void BM_IntrusivePool_Creation(benchmark::State& state) {
+  assert::register_output_callback(nullptr, [](void*, const char* message) {
+    std::cerr << "Assertion failed: " << message << std::endl;
+  });
+
+  usize object_size = 0;
+  ClampedAlign object_align = PowerOfTwo(1ULL);
+
+  const auto storage_size = 1'000'000;
+  std::vector<u8> storage(storage_size);
+  auto storage_view = View<u8>(storage.data(), storage.capacity());
+
+  for (auto x : state) {
+    auto mm = IntrusivePool(object_size, object_align, storage_view);
 
     object_size++;
-    object_align *= 2;
+    object_align = object_align->next();
   }
 }
 
 static void BM_IntrusivePool_Synchronized(benchmark::State& state) {
-  usize object_size = 0;
-  usize object_align = 1;
+  assert::register_output_callback(nullptr, [](void*, const char* message) {
+    std::cerr << "Assertion failed: " << message << std::endl;
+  });
 
-  for (auto _ : state) {
+  usize object_size = 0;
+  ClampedAlign object_align = PowerOfTwo(1ULL);
+
+  const auto storage_size = 1'000'000;
+  std::vector<u8> storage(storage_size);
+  auto storage_view = View<u8>(storage.data(), storage.capacity());
+
+  for (auto x : state) {
     auto options = BenchmarkOptions(object_size, object_size, object_align, object_align);
-    auto mm = IntrusivePool(object_size, object_align);
+
+    auto mm = IntrusivePool(object_size, object_align, storage_view);
     synchronized_benchmark(mm, options);
 
     object_size++;
-    object_align *= 2;
+    object_align = object_align->next();
   }
 }
 
 static void BM_IntrusivePool_Unsynchronized(benchmark::State& state) {
+  assert::register_output_callback(nullptr, [](void*, const char* message) {
+    std::cerr << "Assertion failed: " << message << std::endl;
+  });
+
   usize object_size = 0;
-  usize object_align = 1;
+  ClampedAlign object_align = PowerOfTwo(1ULL);
+
+  const auto storage_size = 1'000'000;
+  std::vector<u8> storage(storage_size);
+  auto storage_view = View<u8>(storage.data(), storage.capacity());
 
   for (auto x : state) {
     auto options = BenchmarkOptions(object_size, object_size, object_align, object_align);
-    auto mm = IntrusivePool(object_size, object_align);
+
+    auto mm = IntrusivePool(object_size, object_align, storage_view);
     unsynchronized_benchmark(mm, options);
 
     object_size++;
-    object_align *= 2;
+    object_align = object_align->next();
   }
 }
 
