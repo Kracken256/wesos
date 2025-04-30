8d31f62624ec4e409dc663037cdf4d8a3ae4f868
Wesley Jones <wesjones2004@gmail.com>
2025-04-29 22:28:13 -0400

    wesos-types: Update Nullable wrapper

--- src/func/libwesos-types/include/wesos-types/Nullable.hh
+++ src/func/libwesos-types/include/wesos-types/Nullable.hh
@@ -8,80 +8,106 @@
 #pragma once
 
 #include <wesos-assert/Assert.hh>
-#include <wesos-types/Array.hh>
+#include <wesos-builtin/Move.hh>
 #include <wesos-types/Null.hh>
-#include <wesos-types/Numeric.hh>
 
 namespace wesos::types {
   template <class ValueGeneric>
   class Nullable {
     union Payload {
-      Array<u8, sizeof(ValueGeneric)> m_bytes;
+      int m_placeholder;
       ValueGeneric m_obj;
 
-      constexpr Payload() : m_bytes{} {}
+      constexpr Payload() : m_placeholder{} {}
     } m_value;
     bool m_isset = false;
+
+    [[nodiscard]] constexpr auto get() const -> const ValueGeneric& { return m_value.m_obj; }
+    [[nodiscard]] constexpr auto get() -> ValueGeneric& { return m_value.m_obj; }
 
   public:
     constexpr Nullable() = default;
     constexpr Nullable(Null) {}
-    constexpr Nullable(ValueGeneric x) : m_isset(true) { m_value.m_obj = x; }
-    constexpr Nullable(const Nullable& other) = default;
-    constexpr Nullable(Nullable&& other) = default;
-    constexpr auto operator=(const Nullable& other) -> Nullable& = default;
-    constexpr auto operator=(Nullable&& other) -> Nullable& = default;
-    constexpr auto operator<=>(const Nullable& other) const = default;
-    constexpr auto operator=(ValueGeneric x) -> Nullable& {
-      m_isset = true;
-      m_value.m_obj = x;
-      return *this;
+    constexpr Nullable(ValueGeneric x) { assign(move(x)); }
+    constexpr Nullable(const Nullable&) = default;
+    constexpr Nullable(Nullable&& o) {
+      if (o.isset()) {
+        assign(move(o.get()));
+        o.unset();
+      }
     }
-
+    constexpr auto operator=(const Nullable&) -> Nullable& = default;
+    constexpr auto operator=(Nullable&& o) -> Nullable& {
+      if (o.isset()) {
+        assign(move(o.get()));
+        o.unset();
+      } else {
+        unset();
+      }
+    }
     constexpr ~Nullable() { unset(); }
 
+    [[nodiscard]] constexpr auto operator<=>(const Nullable&) const = default;
     [[nodiscard]] constexpr auto isset() const -> bool { return m_isset; }
-
-    constexpr operator bool() { return isset(); }
+    [[nodiscard]] constexpr operator bool() { return isset(); }
 
     [[nodiscard]] constexpr auto unwrap() const -> const ValueGeneric& {
-      always_assert(isset());
-      return m_value.m_obj;
+      always_assert(isset(), "nullable is null");
+      return get();
     }
 
     [[nodiscard]] constexpr auto unwrap_unchecked() const -> const ValueGeneric& {
-      return m_value.m_obj;
+      assert_invariant(isset(), "nullable is null");
+      return get();
     }
 
     [[nodiscard]] constexpr auto unwrap() -> ValueGeneric& {
-      always_assert(isset());
-      return m_value.m_obj;
+      always_assert(isset(), "nullable is null");
+      return get();
     }
 
-    [[nodiscard]] constexpr auto unwrap_unchecked() -> ValueGeneric& { return m_value.m_obj; }
+    [[nodiscard]] constexpr auto unwrap_unchecked() -> ValueGeneric& {
+      assert_invariant(isset(), "nullable is null");
+      return get();
+    }
 
     [[nodiscard]] constexpr auto value_or(ValueGeneric&& y) const -> ValueGeneric {
-      return isset() ? m_value.m_obj : y;
+      return isset() ? get() : y;
     }
 
-    void unset() {
+    constexpr auto operator=(ValueGeneric x) -> Nullable& {
+      assign(move(x));
+      return *this;
+    }
+
+    constexpr auto assign(ValueGeneric x) -> Nullable& {
+      unset();
+      m_value.m_obj = move(x);
+      m_isset = true;
+
+      return *this;
+    }
+
+    constexpr auto unset() -> Nullable& {
       if (isset()) {
-        m_value.m_obj.~ValueGeneric();
+        get().~ValueGeneric();
         m_isset = false;
       }
+
+      return *this;
     }
 
-    constexpr void anew() {
+    constexpr auto anew() -> ValueGeneric& {
       unset();
-      m_value.m_obj = ValueGeneric();
-      m_isset = true;
+      return assign({});
     }
 
-    constexpr void fill() {
+    constexpr auto ensure() -> ValueGeneric& {
       if (!isset()) {
-        m_value.m_obj = ValueGeneric();
-        m_isset = true;
+        assign({});
       }
+
+      return get();
     }
   };
 }  // namespace wesos::types
