ab4978d009073f7d8f48084280c05d40d66f68ab
Wesley Jones <wesjones2004@gmail.com>
2025-05-04 01:08:00 -0400

    work

--- src/libs/libwesos-builtin/include/wesos-builtin/Move.hh
+++ src/libs/libwesos-builtin/include/wesos-builtin/Move.hh
@@ -10,8 +10,8 @@
 #include <wesos-types/Template.hh>
 
 namespace wesos {
-  template <typename TGeneric>
-  constexpr auto move(TGeneric&& t) -> types::remove_reference_t<TGeneric>&& {
-    return static_cast<types::remove_reference_t<TGeneric>&&>(t);
+  template <typename T>
+  constexpr auto move(T&& t) -> types::remove_reference_t<T>&& {
+    return static_cast<types::remove_reference_t<T>&&>(t);
   }
 }  // namespace wesos
