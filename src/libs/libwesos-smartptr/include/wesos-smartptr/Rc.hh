4d56423836e18c0330791dd5e02eaf804cc42abe
Wesley Jones <wesjones2004@gmail.com>
2025-05-04 04:48:11 -0400

    wesos-smartptr: Rc<T> class (non atomic)

--- src/libs/libwesos-smartptr/include/wesos-smartptr/Rc.hh
+++ src/libs/libwesos-smartptr/include/wesos-smartptr/Rc.hh
@@ -1,82 +1,175 @@
-// /**
-//  * This file is part of the WesOS project.
-//  *
-//  * WesOS is public domain software: you can redistribute it and/or modify
-//  * it under the terms of the Unlicense(https://unlicense.org/).
-//  */
+/**
+ * This file is part of the WesOS project.
+ *
+ * WesOS is public domain software: you can redistribute it and/or modify
+ * it under the terms of the Unlicense(https://unlicense.org/).
+ */
 
-// #pragma once
+#pragma once
 
-// #include <wesos-mem/IntrusivePool.hh>  /// TODO: Remove me
-// #include <wesos-mem/MemoryResourceProtocol.hh>
-// #include <wesos-types/NullableOwnPtr.hh>
+#include <wesos-builtin/Range.hh>
+#include <wesos-mem/MemoryResourceProtocol.hh>
+#include <wesos-types/NullableOwnPtr.hh>
+#include <wesos-types/Template.hh>
 
-// namespace wesos::smartptr {
-//   template <class Pointee>
-//   class Rc {
-//     NullableOwnPtr<Pointee> m_ptr;
-//     usize& m_rc;
-//     mem::MemoryResourceProtocol* m_pmr;
+namespace wesos::smartptr {
+  template <class Object>
+  class Rc {
+    struct State {
+      usize m_state_rc = 1;
+      usize m_data_rc = 1;
+      mem::MemoryResourceProtocol& m_pmr;
 
-//     Rc(OwnPtr<Pointee> ptr, usize& rc, mem::MemoryResourceProtocol& pmr)
-//         : m_ptr(ptr), m_rc(rc), m_pmr(&pmr) {}
+      constexpr State(mem::MemoryResourceProtocol& pmr) : m_pmr(pmr) {}
+    };
 
-//   public:
-//     constexpr Rc(const Rc& o) : m_ptr(o.m_ptr), m_rc(++o.m_rc), m_pmr(o.m_pmr){};
+    NullableRefPtr<Object> m_ptr;
+    NullableRefPtr<State> m_state;
 
-//     constexpr Rc(Rc&& o) : m_ptr(o.m_ptr), m_rc(o.m_rc), m_pmr(o.m_pmr) {
-//       o.m_ptr = nullptr;
-//       o.m_rc = 0;
-//     };
+    constexpr Rc(OwnPtr<Object> ptr, OwnPtr<State> state) : m_ptr(ptr), m_state(state) {}
 
-//     constexpr auto operator=(const Rc& o) -> Rc& {
-//       m_ptr = o.m_ptr;
-//       m_rc = ++o.m_rc;
-//       m_pmr = o.m_pmr;
-//     };
+  public:
+    constexpr Rc(const Rc& o) : m_ptr(o.m_ptr), m_state(o.m_state) {
+      ++m_state->m_state_rc;
+      ++m_state->m_data_rc;
+    }
 
-//     constexpr auto operator=(Rc&& o) -> Rc& {
-//       m_ptr = o.m_ptr;
-//       m_rc = o.m_rc;
-//       m_pmr = o.m_pmr;
+    constexpr auto operator=(const Rc& o) -> Rc& {
+      m_ptr = o.m_ptr;
+      m_state = o.m_state;
+      ++m_state->m_state_rc;
+      ++m_state->m_data_rc;
+    }
 
-//       o.m_ptr = nullptr;
-//       o.m_rc = 0;
-//     };
+    constexpr Rc(Rc&& o) : m_ptr(o.m_ptr), m_state(o.m_state) {
+      o.m_ptr = null;
+      o.m_state = null;
+    }
 
-//     ~Rc() {
-//       if (--m_rc == 0) [[unlikely]] {
-//         auto* object_ptr = m_ptr.unwrap();
-//         const auto object_range =
-//             View<u8>(bit_cast<u8*>(object_ptr), sizeof(Pointee));
+    constexpr auto operator=(Rc&& o) -> Rc& {
+      m_ptr = o.m_ptr;
+      m_state = o.m_state;
+      o.m_ptr = null;
+      o.m_state = null;
+    };
 
-//         object_ptr->~Pointee();
-//         m_pmr->deallocate_bytes(object_range, alignof(Pointee));
+    ~Rc() {
+      if (m_state.isset()) {
+        mem::MemoryResourceProtocol& pmr = m_state->m_pmr;
 
-// #ifndef NDEBUG
-//         m_ptr = nullptr;
-// #endif
-//       }
-//     };
+        if (m_ptr.isset() && --m_state->m_data_rc == 0) [[unlikely]] {
+          pmr.template destroy_and_deallocate<Object>(m_ptr.unwrap(), 1);
+        }
 
-//     template <class... Args>
-//     [[nodiscard]] static auto create(mem::MemoryResourceProtocol& pmr) {
-//       return [&pmr](Args... args) -> Nullable<Rc> {
-//         (void)pmr;
-//         return nullptr;
-//         /// TODO:
-//       };
-//     }
+        if (--m_state->m_state_rc == 0) [[unlikely]] {
+          pmr.template destroy_and_deallocate<State>(m_state.unwrap(), 1);
+        }
+      }
+    }
 
-//     /// TODO:
-//   };
+    [[nodiscard]] constexpr auto operator<=>(const Rc& o) const { return unwrap() <=> o.unwrap(); }
+    [[nodiscard]] constexpr auto operator==(types::Null) const { return is_null(); }
+    [[nodiscard]] constexpr auto operator==(types::nullptr_t) const { return is_null(); }
 
-//   static void test() {  //
-//     Array<u8, 4096> bytes;
-//     mem::IntrusivePool pmr(sizeof(int), alignof(int), bytes.as_view());
+    [[nodiscard]] static constexpr auto min_alloc_size() -> usize { return max(sizeof(State), sizeof(Object)); }
 
-//     auto hello = Rc<int>::create(pmr)().unwrap();
+    [[nodiscard]] static constexpr auto min_alloc_alignment() -> PowerOfTwo<usize> {
+      return max(alignof(State), alignof(Object));
+    }
 
-//     constexpr auto sz = sizeof(hello);
-//   }
-// }  // namespace wesos::smartptr
+    template <class... Args>
+    [[nodiscard]] static auto create(mem::MemoryResourceProtocol& pmr, Args&&... args) -> Nullable<Rc> {
+      auto inner_object = pmr.allocate_storage<Object>(1);
+      if (inner_object.is_null()) [[unlikely]] {
+        return null;
+      }
+
+      auto state_object = pmr.allocate_storage<State>(1);
+      if (state_object.is_null()) [[unlikely]] {
+        return null;
+      }
+
+      ::new (static_cast<void*>(inner_object.unwrap())) Object(forward<Args>(args)...);
+      ::new (static_cast<void*>(state_object.unwrap())) State(pmr);
+
+      return Rc(inner_object.get_unchecked(), state_object.get_unchecked());
+    }
+
+    ///=========================================================================================
+    /// LIFETIME MANAGEMENT
+    ///=========================================================================================
+
+    [[nodiscard]] constexpr auto ref_count() const -> usize { return m_state.isset() ? m_state->m_data_rc : 0; }
+
+    constexpr auto disown() -> void {
+      mem::MemoryResourceProtocol& pmr = m_state->m_pmr;
+
+      if (m_state->m_data_rc > 0) {
+        m_state->m_data_rc = 0;
+        pmr.template destroy_and_deallocate<Object>(m_ptr.unwrap(), 1);
+      }
+
+      m_ptr = null;
+    }
+
+    ///=========================================================================================
+    /// POINTER ACCESS
+    ///=========================================================================================
+
+    [[nodiscard]] constexpr auto unwrap() -> Object* { return m_ptr.unwrap(); }
+    [[nodiscard]] constexpr auto unwrap() const -> const Object* { return m_ptr.unwrap(); }
+
+    [[nodiscard]] constexpr auto isset() const -> bool { return m_ptr.isset(); }
+    [[nodiscard]] constexpr auto is_null() const -> bool { return m_ptr.is_null(); }
+
+    template <class U = Object>
+    [[nodiscard]] constexpr auto operator->() -> U* requires(!types::is_same_v<U, void>) {
+      assert_always(m_ptr.isset());
+      return unwrap();
+    };
+
+    template <class U = Object>
+    [[nodiscard]] constexpr auto operator->() const -> const U* requires(!types::is_same_v<U, void>) {
+      assert_always(m_ptr.isset());
+      return unwrap();
+    };
+
+    template <class U = Object>
+    [[nodiscard]] constexpr auto operator*() -> U& requires(!types::is_same_v<U, void>) {
+      assert_always(m_ptr.isset());
+      return *unwrap();
+    };
+
+    template <class U = Object>
+    [[nodiscard]] constexpr auto operator*() const -> const U& requires(!types::is_same_v<U, void>) {
+      assert_always(m_ptr.isset());
+      return *unwrap();
+    };
+
+    template <class U = Object>
+    [[nodiscard]] constexpr auto get() -> U& requires(!types::is_same_v<U, void>) {
+      assert_always(m_ptr.isset());
+      return *unwrap();
+    };
+
+    template <class U = Object>
+    [[nodiscard]] constexpr auto get() const -> const U& requires(!types::is_same_v<U, void>) {
+      assert_always(m_ptr.isset());
+      return *unwrap();
+    };
+
+    template <class U = Object>
+    [[nodiscard]] constexpr auto get_unchecked() -> U& requires(!types::is_same_v<U, void>) {
+      assert_invariant(m_ptr.isset());
+      return *unwrap();
+    };
+
+    template <class U = Object>
+    [[nodiscard]] constexpr auto get_unchecked() const -> const U& requires(!types::is_same_v<U, void>) {
+      assert_invariant(m_ptr.isset());
+      return *unwrap();
+    };
+  };
+
+  static_assert(sizeof(Rc<void>) == sizeof(void*) * 2);
+}  // namespace wesos::smartptr
--- src/libs/libwesos-smartptr/test/Rc.cc
+++ src/libs/libwesos-smartptr/test/Rc.cc
@@ -7,4 +7,204 @@
 
 #include <gtest/gtest.h>
 
+#include <wesos-mem/IntrusivePool.hh>
 #include <wesos-smartptr/Rc.hh>
+
+using namespace wesos;
+using namespace wesos::smartptr;
+
+class SemanticCounter final {
+  usize &m_constructed, &m_moved, &m_copied, &m_destructed;
+
+public:
+  SemanticCounter(usize &constructed, usize &moved, usize &copied, usize &destructed)
+      : m_constructed(constructed), m_moved(moved), m_copied(copied), m_destructed(destructed) {
+    ++m_constructed;
+  }
+  SemanticCounter(const SemanticCounter &o)
+      : m_constructed(o.m_constructed), m_moved(o.m_moved), m_copied(o.m_copied), m_destructed(o.m_destructed) {
+    ++m_copied;
+  }
+  SemanticCounter(SemanticCounter &&o)
+      : m_constructed(o.m_constructed), m_moved(o.m_moved), m_copied(o.m_copied), m_destructed(o.m_destructed) {
+    ++m_moved;
+  };
+
+  ~SemanticCounter() { ++m_destructed; }
+};
+
+TEST(Rc, CreateIntRc) {
+  const auto buf_size = 64;
+  const auto value = 42;
+
+  auto bytes = Array<u8, buf_size>();
+  auto pmr = mem::IntrusivePool(Rc<int>::min_alloc_size(), Rc<int>::min_alloc_alignment(), bytes.as_view());
+
+  auto int_rc_maybe = Rc<int>::create(pmr, value);
+  ASSERT_NE(int_rc_maybe, null);
+  auto int_rc = move(int_rc_maybe.value());
+
+  EXPECT_EQ(int_rc.get(), value);
+  EXPECT_EQ(int_rc.ref_count(), 1);
+
+  *int_rc = value + 1;
+
+  EXPECT_EQ(*int_rc, value + 1);
+  EXPECT_EQ(int_rc.ref_count(), 1);
+}
+
+TEST(Rc, ArgumentForwarding) {
+  const auto buf_size = 64;
+  auto bytes = Array<u8, buf_size>();
+  auto pmr = mem::IntrusivePool(Rc<SemanticCounter>::min_alloc_size(), Rc<SemanticCounter>::min_alloc_alignment(),
+                                bytes.as_view());
+
+  usize constructed = 0;
+  usize moved = 0;
+  usize copied = 0;
+  usize destructed = 0;
+
+  {
+    auto rc_maybe = Rc<SemanticCounter>::create(pmr, SemanticCounter(constructed, moved, copied, destructed));
+    ASSERT_NE(rc_maybe, null);
+    auto rc = move(rc_maybe.value());
+
+    EXPECT_EQ(rc.ref_count(), 1);
+
+    auto rc_copy = rc;  // NOLINT(performance-unnecessary-copy-initialization)
+
+    EXPECT_EQ(rc.ref_count(), 2);
+    EXPECT_EQ(rc_copy.ref_count(), 2);
+
+    EXPECT_EQ(constructed, 1);
+    EXPECT_EQ(copied, 0);
+    EXPECT_EQ(moved, 1);
+    EXPECT_EQ(destructed, 1);
+  }
+
+  EXPECT_EQ(constructed, 1);
+  EXPECT_EQ(copied, 0);
+  EXPECT_EQ(moved, 1);
+  EXPECT_EQ(destructed, 2);
+}
+
+TEST(Rc, NoCopy) {
+  static_assert(sizeof(SemanticCounter) >= mem::IntrusivePool::minimum_size());
+
+  const auto buf_size = 64;
+  auto bytes = Array<u8, buf_size>();
+  auto pmr = mem::IntrusivePool(Rc<SemanticCounter>::min_alloc_size(), Rc<SemanticCounter>::min_alloc_alignment(),
+                                bytes.as_view());
+
+  usize constructed = 0;
+  usize moved = 0;
+  usize copied = 0;
+  usize destructed = 0;
+
+  {
+    auto rc_maybe = Rc<SemanticCounter>::create(pmr, constructed, moved, copied, destructed);
+    ASSERT_NE(rc_maybe, null);
+    auto rc = move(rc_maybe.value());
+
+    EXPECT_EQ(copied, 0);
+  }
+
+  EXPECT_EQ(copied, 0);
+}
+
+TEST(Rc, NoMove) {
+  static_assert(sizeof(SemanticCounter) >= mem::IntrusivePool::minimum_size());
+
+  const auto buf_size = 64;
+  auto bytes = Array<u8, buf_size>();
+  auto pmr = mem::IntrusivePool(Rc<SemanticCounter>::min_alloc_size(), Rc<SemanticCounter>::min_alloc_alignment(),
+                                bytes.as_view());
+
+  usize constructed = 0;
+  usize moved = 0;
+  usize copied = 0;
+  usize destructed = 0;
+
+  {
+    auto rc_maybe = Rc<SemanticCounter>::create(pmr, constructed, moved, copied, destructed);
+    ASSERT_NE(rc_maybe, null);
+    auto rc = move(rc_maybe.value());
+
+    EXPECT_EQ(moved, 0);
+  }
+
+  EXPECT_EQ(moved, 0);
+}
+
+TEST(Rc, Disown) {
+  const auto buf_size = 64;
+  auto bytes = Array<u8, buf_size>();
+  auto pmr = mem::IntrusivePool(Rc<SemanticCounter>::min_alloc_size(), Rc<SemanticCounter>::min_alloc_alignment(),
+                                bytes.as_view());
+
+  usize constructed = 0;
+  usize moved = 0;
+  usize copied = 0;
+  usize destructed = 0;
+
+  Nullable<Rc<SemanticCounter>> ref_a;
+
+  {
+    Nullable<Rc<SemanticCounter>> ref_b;
+
+    {
+      Nullable<Rc<SemanticCounter>> ref_c;
+
+      {
+        auto rc_maybe = Rc<SemanticCounter>::create(pmr, SemanticCounter(constructed, moved, copied, destructed));
+        ASSERT_NE(rc_maybe, null);
+        auto rc = move(rc_maybe.value());
+
+        EXPECT_EQ(rc.ref_count(), 1);
+
+        ref_a = rc;
+        ref_b = rc;
+        ref_c = rc;
+
+        EXPECT_EQ(rc.ref_count(), 4);
+        EXPECT_EQ(ref_a.value().ref_count(), 4);
+        EXPECT_EQ(ref_b.value().ref_count(), 4);
+        EXPECT_EQ(ref_c.value().ref_count(), 4);
+
+        EXPECT_EQ(constructed, 1);
+        EXPECT_EQ(copied, 0);
+        EXPECT_EQ(moved, 1);
+        EXPECT_EQ(destructed, 1);
+      }
+
+      EXPECT_EQ(constructed, 1);
+      EXPECT_EQ(copied, 0);
+      EXPECT_EQ(moved, 1);
+      EXPECT_EQ(destructed, 1);
+
+      EXPECT_EQ(ref_a.value().ref_count(), 3);
+      EXPECT_EQ(ref_b.value().ref_count(), 3);
+      EXPECT_EQ(ref_c.value().ref_count(), 3);
+    }
+
+    EXPECT_EQ(constructed, 1);
+    EXPECT_EQ(copied, 0);
+    EXPECT_EQ(moved, 1);
+    EXPECT_EQ(destructed, 1);
+
+    EXPECT_EQ(ref_a.value().ref_count(), 2);
+    EXPECT_EQ(ref_b.value().ref_count(), 2);
+
+    ref_b->disown();
+
+    EXPECT_EQ(ref_a.value().ref_count(), 0);
+    EXPECT_EQ(ref_b.value().ref_count(), 0);
+  }
+
+  EXPECT_EQ(ref_a.value().ref_count(), 0);
+
+  EXPECT_EQ(constructed, 1);
+  EXPECT_EQ(copied, 0);
+  EXPECT_EQ(moved, 1);
+  EXPECT_EQ(destructed, 2);
+}
