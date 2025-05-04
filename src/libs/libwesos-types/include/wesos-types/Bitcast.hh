af2d873812708bf46ad8f25d7ef4c09521517b79
Wesley Jones <wesjones2004@gmail.com>
2025-05-03 21:47:35 -0400

    work

--- src/libs/libwesos-types/include/wesos-types/Bitcast.hh
+++ src/libs/libwesos-types/include/wesos-types/Bitcast.hh
@@ -11,19 +11,9 @@
 
 namespace wesos {
   template <typename ToGeneric, typename FromGeneric>
-  constexpr auto bit_cast(const FromGeneric& src) -> ToGeneric {
-    static_assert(sizeof(ToGeneric) == sizeof(FromGeneric), "bit_cast requires equal sizes");
-    /// TODO: Implement these checks
-
-    // static_assert(std::is_trivially_copyable_v<FromGeneric>,
-    //               "bit_cast requires trivially copyable From");
-    // static_assert(std::is_trivially_copyable_v<ToGeneric>,
-    //               "bit_cast requires trivially copyable ToGeneric");
-    // static_assert(std::is_trivially_constructible_v<ToGeneric>,
-    //               "bit_cast requires trivially constructible ToGeneric");
-
-    ToGeneric dst;
-    __builtin_memcpy(&dst, &src, sizeof(ToGeneric));  // safe type punning
-    return dst;
+  [[nodiscard]] constexpr auto bit_cast(const FromGeneric& from) -> ToGeneric
+    requires(sizeof(ToGeneric) == sizeof(FromGeneric)) && __is_trivially_copyable
+  (ToGeneric) && __is_trivially_copyable(FromGeneric) {
+    return __builtin_bit_cast(ToGeneric, from);
   }
 }  // namespace wesos
