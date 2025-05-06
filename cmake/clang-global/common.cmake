bd12f0e23ef95018a5827e924d85d40ee9017493
Wesley Jones <wesjones2004@gmail.com>
2025-05-06 05:11:33 -0400

    work

--- cmake/clang-global/common.cmake
+++ cmake/clang-global/common.cmake
@@ -1,7 +1,7 @@
 ################################################################################
 # LINTING AND REPORTING
 ################################################################################
-add_compile_options(-Wall -Wextra -Wpedantic -Werror -Wold-style-cast)
+add_compile_options(-Wall -Wextra -Wpedantic -Werror -Wold-style-cast -Wno-unused-command-line-argument)
 add_compile_options(-Wshadow -Wconversion -Wsign-conversion)
 add_compile_options(-Wno-error=unused-parameter -Wno-error=unused-variable -Wno-error=unused-function)
 add_compile_options(-Wno-error=deprecated -g3)
@@ -18,7 +18,7 @@ add_compile_options(-fno-pic)
 add_compile_options(-fno-omit-frame-pointer) # Concepts like "backtrace()" require this.
 add_compile_options(-fno-rtti -fno-exceptions -fno-strict-aliasing) # Because I want it to work.
 add_compile_options(-nostdlib -fno-asynchronous-unwind-tables -fno-unwind-tables)
-add_compile_options(-Wno-unused-command-line-argument)
+add_compile_options(-Wl,--gc-sections)
 add_compile_options(-fvisibility=hidden -fvisibility-inlines-hidden)
 
 if(WESOS_TARGET_TRIPLE)
