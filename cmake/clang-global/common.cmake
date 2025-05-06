effe0cff94f2cd6b84179fdc7f257a4df7cea107
Wesley Jones <wesjones2004@gmail.com>
2025-05-05 22:38:35 -0400

    cmake: build kernel with specified target triple

--- cmake/clang-global/common.cmake
+++ cmake/clang-global/common.cmake
@@ -20,6 +20,7 @@ add_compile_options(-fno-rtti -fno-exceptions -fno-strict-aliasing) # Because I 
 add_compile_options(-nostdlib -fno-asynchronous-unwind-tables -fno-unwind-tables)
 add_compile_options(-Wno-unused-command-line-argument)
 add_compile_options(-fvisibility=hidden -fvisibility-inlines-hidden)
+add_compile_options(-target ${WESOS_TARGET_TRIPLE})
 
 set(CMAKE_CXX_STANDARD 20)
 set(CMAKE_CXX_STANDARD_REQUIRED ON)
