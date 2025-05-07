ca3c30ea73e86f9f4fd076f9e2de26abc9d6c5ba
Wesley Jones <wesjones2004@gmail.com>
2025-05-06 20:57:58 -0400

    cmake: Support LTO

--- cmake/clang-global/release-mode.cmake
+++ cmake/clang-global/release-mode.cmake
@@ -4,3 +4,8 @@ include(clang-global/common)
 # OPTIMIZATION
 ################################################################################
 add_compile_options(-O3 -DNDEBUG)
+
+if(WESOS_WORLD_LTO)
+  add_compile_options(-flto)
+  add_link_options(-flto)
+endif()
