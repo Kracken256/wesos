eb391072ea91b833d1a0ba532ac9bc2de9389d64
Wesley Jones <wesjones2004@gmail.com>
2025-05-09 00:02:10 -0400

    wesos-types: is_integral_v

--- src/libs/libwesos-types/include/wesos-types/Template.hh
+++ src/libs/libwesos-types/include/wesos-types/Template.hh
@@ -246,5 +246,76 @@ namespace wesos::types {
 
   template <typename From, typename To>
   inline constexpr bool is_convertible_v = is_convertible<From, To>::value;
+
+  ///===========================================================================
+
+  template <typename T>
+  struct is_integral {
+    static constexpr bool value = false;
+  };
+
+  template <>
+  struct is_integral<bool> {
+    static constexpr bool value = true;
+  };
+  template <>
+  struct is_integral<char> {
+    static constexpr bool value = true;
+  };
+  template <>
+  struct is_integral<signed char> {
+    static constexpr bool value = true;
+  };
+  template <>
+  struct is_integral<unsigned char> {
+    static constexpr bool value = true;
+  };
+  template <>
+  struct is_integral<wchar_t> {
+    static constexpr bool value = true;
+  };
+  template <>
+  struct is_integral<char16_t> {
+    static constexpr bool value = true;
+  };
+  template <>
+  struct is_integral<char32_t> {
+    static constexpr bool value = true;
+  };
+  template <>
+  struct is_integral<short> {
+    static constexpr bool value = true;
+  };
+  template <>
+  struct is_integral<unsigned short> {
+    static constexpr bool value = true;
+  };
+  template <>
+  struct is_integral<int> {
+    static constexpr bool value = true;
+  };
+  template <>
+  struct is_integral<unsigned int> {
+    static constexpr bool value = true;
+  };
+  template <>
+  struct is_integral<long> {
+    static constexpr bool value = true;
+  };
+  template <>
+  struct is_integral<unsigned long> {
+    static constexpr bool value = true;
+  };
+  template <>
+  struct is_integral<long long> {
+    static constexpr bool value = true;
+  };
+  template <>
+  struct is_integral<unsigned long long> {
+    static constexpr bool value = true;
+  };
+
+  template <typename T>
+  inline constexpr bool is_integral_v = is_integral<T>::value;
   // NOLINTEND(readability-identifier-naming)
 }  // namespace wesos::types
