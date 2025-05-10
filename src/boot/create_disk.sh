beaab9cccb370328e2fe3df28eb65878f680b3df
Wesley Jones <wesjones2004@gmail.com>
2025-05-09 22:31:02 -0400

    Create ISO disk image

--- src/boot/create_disk.sh
+++ src/boot/create_disk.sh
@@ -2,9 +2,8 @@
 
 ISO_NAME="WesOS Live.iso"
 
-# Create zeroed 32MB raw image
 # WARNING: The EFI File System type depends on the size of the image file.
-dd if=/dev/zero of=drive.hdd bs=1k count=32000 status=none
+dd if=/dev/zero of=drive.hdd bs=1000 count=256000 status=none
 if [ $? -ne 0 ]; then
   echo "Failed to create image file"
   exit 1
@@ -27,10 +26,14 @@ if [ $? -ne 0 ]; then
   exit 1
 fi
 
-# Create a EFI FAT16 partition
-parted -s $DEVICE_PATH mkpart primary fat16 1MiB 100%
+###########################################################################################
+# START: CREATE EFI FAT12 PARTITION
+###########################################################################################
+
+# Create a FAT12 EFI partition 5 MiB in size
+parted -s $DEVICE_PATH mkpart primary 1MiB 6MiB
 if [ $? -ne 0 ]; then
-  echo "Failed to create FAT16 partition"
+  echo "Failed to create FAT12 partition"
   losetup -d $DEVICE_PATH
   rm -f drive.hdd
   exit 1
@@ -54,10 +57,10 @@ if [ $? -ne 0 ]; then
   exit 1
 fi
 
-# Format the partition with FAT16
-mkfs.fat -F16 ${DEVICE_PATH}p1 >/dev/null
+# Format the partition with FAT12
+mkfs.fat -F12 -n "EFI" ${DEVICE_PATH}p1 >/dev/null
 if [ $? -ne 0 ]; then
-  echo "Failed to format partition with FAT16"
+  echo "Failed to format partition with FAT12"
   losetup -d $DEVICE_PATH
   rm -f drive.hdd
   exit 1
@@ -83,7 +86,7 @@ if [ $? -ne 0 ]; then
 fi
 
 # Setup the EFI directory structure
-mkdir -p $TEMP_MOUNT_POINT/EFI/BOOT
+mkdir -p $TEMP_MOUNT_POINT/EFI/BOOT $TEMP_MOUNT_POINT/WESOS
 if [ $? -ne 0 ]; then
   echo "Failed to create EFI directory structure"
   umount $TEMP_MOUNT_POINT
@@ -115,6 +118,185 @@ if [ $? -ne 0 ]; then
 fi
 
 rmdir $TEMP_MOUNT_POINT
+
+###########################################################################################
+# END: CREATE EFI FAT12 PARTITION
+###########################################################################################
+
+###########################################################################################
+# START: CREATE KERNEL MODULE PARTITION
+###########################################################################################
+
+# Create an FAT32 partition 64 MiB in size
+parted -s $DEVICE_PATH mkpart primary 6MiB 70MiB
+if [ $? -ne 0 ]; then
+  echo "Failed to create FAT32 partition"
+  losetup -d $DEVICE_PATH
+  rm -f drive.hdd
+  exit 1
+fi
+
+# Set the partition name to "MODULE"
+parted -s $DEVICE_PATH name 2 "MODULE"
+if [ $? -ne 0 ]; then
+  echo "Failed to set partition name to MODULE"
+  losetup -d $DEVICE_PATH
+  rm -f drive.hdd
+  exit 1
+fi
+
+# Set the partition type to basic data
+parted -s $DEVICE_PATH set 2 msftdata on
+if [ $? -ne 0 ]; then
+  echo "Failed to set partition type to basic data"
+  losetup -d $DEVICE_PATH
+  rm -f drive.hdd
+  exit 1
+fi
+
+# Format the partition with FAT32
+mkfs.fat -F32 -n "MODULE" ${DEVICE_PATH}p2 >/dev/null
+if [ $? -ne 0 ]; then
+  echo "Failed to format partition with FAT32"
+  losetup -d $DEVICE_PATH
+  rm -f drive.hdd
+  exit 1
+fi
+
+# Create a mount point for the FAT32 partition
+TEMP_MOUNT_POINT=$(mktemp -d)
+if [ $? -ne 0 ]; then
+  echo "Failed to create mount point for FAT32 partition"
+  losetup -d $DEVICE_PATH
+  rm -f drive.hdd
+  exit 1
+fi
+
+# Mount the FAT32 partition
+mount ${DEVICE_PATH}p2 $TEMP_MOUNT_POINT
+if [ $? -ne 0 ]; then
+  echo "Failed to mount FAT32 partition"
+  rmdir $TEMP_MOUNT_POINT
+  losetup -d $DEVICE_PATH
+  rm -f drive.hdd
+  exit 1
+fi
+
+# Set up the MODULE directory structure
+mkdir -p $TEMP_MOUNT_POINT/modules
+if [ $? -ne 0 ]; then
+  echo "Failed to create MODULE directory structure"
+  umount $TEMP_MOUNT_POINT
+  rmdir $TEMP_MOUNT_POINT
+  losetup -d $DEVICE_PATH
+  rm -f drive.hdd
+  exit 1
+fi
+
+# Unmount the FAT32 partition
+umount $TEMP_MOUNT_POINT
+if [ $? -ne 0 ]; then
+  echo "Failed to unmount FAT32 partition"
+  rmdir $TEMP_MOUNT_POINT
+  losetup -d $DEVICE_PATH
+  rm -f drive.hdd
+  exit 1
+fi
+
+rmdir $TEMP_MOUNT_POINT
+
+###########################################################################################
+# END: CREATE KERNEL MODULE PARTITION
+###########################################################################################
+
+###########################################################################################
+# START: CREATE APPLICATION PARTITION
+###########################################################################################
+
+# Create an EXT4 partition 1 GiB in size
+parted -s $DEVICE_PATH mkpart primary 70MiB 100%
+if [ $? -ne 0 ]; then
+  echo "Failed to create EXT4 partition"
+  losetup -d $DEVICE_PATH
+  rm -f drive.hdd
+  exit 1
+fi
+
+# Set the partition name to "APPLICATION"
+parted -s $DEVICE_PATH name 3 "APPLICATION"
+if [ $? -ne 0 ]; then
+  echo "Failed to set partition name to APPLICATION"
+  losetup -d $DEVICE_PATH
+  rm -f drive.hdd
+  exit 1
+fi
+
+# Set the partition type to basic data
+parted -s $DEVICE_PATH set 3 msftdata on
+if [ $? -ne 0 ]; then
+  echo "Failed to set partition type to basic data"
+  losetup -d $DEVICE_PATH
+  rm -f drive.hdd
+  exit 1
+fi
+
+# Format the partition with EXT4
+mkfs.ext4 -L "APPLICATION" ${DEVICE_PATH}p3 >/dev/null
+if [ $? -ne 0 ]; then
+  echo "Failed to format partition with EXT4"
+  losetup -d $DEVICE_PATH
+  rm -f drive.hdd
+  exit 1
+fi
+
+# Create a mount point for the EXT4 partition
+TEMP_MOUNT_POINT=$(mktemp -d)
+if [ $? -ne 0 ]; then
+  echo "Failed to create mount point for EXT4 partition"
+  losetup -d $DEVICE_PATH
+  rm -f drive.hdd
+  exit 1
+fi
+
+# Mount the EXT4 partition
+mount ${DEVICE_PATH}p3 $TEMP_MOUNT_POINT
+if [ $? -ne 0 ]; then
+  echo "Failed to mount EXT4 partition"
+  rmdir $TEMP_MOUNT_POINT
+  losetup -d $DEVICE_PATH
+  rm -f drive.hdd
+  exit 1
+fi
+
+# Set up the APPLICATION directory structure
+mkdir -p $TEMP_MOUNT_POINT/applications
+if [ $? -ne 0 ]; then
+  echo "Failed to create APPLICATION directory structure"
+  umount $TEMP_MOUNT_POINT
+  rmdir $TEMP_MOUNT_POINT
+  losetup -d $DEVICE_PATH
+  rm -f drive.hdd
+  exit 1
+fi
+
+# Unmount the EXT4 partition
+umount $TEMP_MOUNT_POINT
+if [ $? -ne 0 ]; then
+  echo "Failed to unmount EXT4 partition"
+  rmdir $TEMP_MOUNT_POINT
+  losetup -d $DEVICE_PATH
+  rm -f drive.hdd
+  exit 1
+fi
+
+rmdir $TEMP_MOUNT_POINT
+
+###########################################################################################
+# END: CREATE APPLICATION PARTITION
+###########################################################################################
+
+
+
 
 # Detach the loop device
 losetup -d $DEVICE_PATH
