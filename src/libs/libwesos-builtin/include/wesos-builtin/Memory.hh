a218cd59b02fbf7584bd3e674a42a7786772bf9e
Wesley Jones <wesjones2004@gmail.com>
2025-05-10 21:41:12 -0400

    Inline it

--- src/libs/libwesos-builtin/include/wesos-builtin/Memory.hh
+++ src/libs/libwesos-builtin/include/wesos-builtin/Memory.hh
@@ -12,9 +12,39 @@
 namespace wesos {
   /// NOLINTBEGIN(readability-identifier-naming)
 
-  extern "C" auto memcpy(void *dest, const void *src, usize n) -> void *;
-  extern "C" auto memset(void *dest, int value, usize n) -> void *;
-  extern "C" auto memmove(void *dest, const void *src, usize n) -> void *;
+  extern "C" inline auto memcpy(void *dest, const void *src, usize n) -> void * {
+    auto *d = static_cast<u8 *>(dest);
+    const auto *s = static_cast<const u8 *>(src);
+    for (usize i = 0; i < n; ++i) {
+      d[i] = s[i];
+    }
+    return dest;
+  }
+
+  extern "C" inline auto memset(void *dest, int value, usize n) -> void * {
+    auto *d = static_cast<u8 *>(dest);
+    for (usize i = 0; i < n; ++i) {
+      d[i] = static_cast<u8>(value);
+    }
+    return dest;
+  }
+
+  extern "C" inline auto memmove(void *dest, const void *src, usize n) -> void * {
+    auto *d = static_cast<u8 *>(dest);
+    const auto *s = static_cast<const u8 *>(src);
+
+    if (d < s) {
+      for (usize i = 0; i < n; ++i) {
+        d[i] = s[i];
+      }
+    } else if (d > s) {
+      for (usize i = n; i != 0; --i) {
+        d[i - 1] = s[i - 1];
+      }
+    }
+
+    return dest;
+  }
 
   /// NOLINTEND(readability-identifier-naming)
 }  // namespace wesos
--- src/libs/libwesos-builtin/src/Memory.cc
+++ src/libs/libwesos-builtin/src/Memory.cc
@@ -8,37 +8,3 @@
 #include <wesos-builtin/Memory.hh>
 
 using namespace wesos;
-
-extern "C" auto memcpy(void *dest, const void *src, usize n) -> void * {
-  auto *d = static_cast<u8 *>(dest);
-  const auto *s = static_cast<const u8 *>(src);
-  for (usize i = 0; i < n; ++i) {
-    d[i] = s[i];
-  }
-  return dest;
-}
-
-extern "C" auto memset(void *dest, int value, usize n) -> void * {
-  auto *d = static_cast<u8 *>(dest);
-  for (usize i = 0; i < n; ++i) {
-    d[i] = static_cast<u8>(value);
-  }
-  return dest;
-}
-
-extern "C" auto memmove(void *dest, const void *src, usize n) -> void * {
-  auto *d = static_cast<u8 *>(dest);
-  const auto *s = static_cast<const u8 *>(src);
-
-  if (d < s) {
-    for (usize i = 0; i < n; ++i) {
-      d[i] = s[i];
-    }
-  } else if (d > s) {
-    for (usize i = n; i != 0; --i) {
-      d[i - 1] = s[i - 1];
-    }
-  }
-
-  return dest;
-}
