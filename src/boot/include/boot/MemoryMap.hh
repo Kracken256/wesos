6da8c784f410ad148b12547bfc6fdffc94dc1fdd
Wesley Jones <wesjones2004@gmail.com>
2025-05-10 22:19:58 -0400

    UEFI Get memory map

---
+++ src/boot/include/boot/MemoryMap.hh
@@ -0,0 +1,58 @@
+/**
+ * This file is part of the WesOS project.
+ *
+ * WesOS is public domain software: you can redistribute it and/or modify
+ * it under the terms of the Unlicense(https://unlicense.org/).
+ */
+
+#pragma once
+
+#include <boot/EFI.hh>
+#include <boot/Logger.hh>
+#include <wesos-types/Types.hh>
+
+namespace wesos::boot::efi {
+  class MemoryMap {
+    MemoryDescriptor *m_mmap = nullptr;
+    usize m_mmap_size = 0;
+    usize m_desc_count = 0;
+    usize m_desc_size = 0;
+    u32 m_desc_version = 0;
+
+    MemoryMap() = default;
+
+    [[nodiscard]] static auto initialize_memory_map_details(MemoryMap &mmap) -> bool;
+    [[nodiscard]] static auto create_memory_map_buffer(MemoryMap &mmap) -> bool;
+    [[nodiscard]] static auto retrieve_memory_map(MemoryMap &mmap) -> bool;
+
+  public:
+    MemoryMap(MemoryMap &&o);
+    MemoryMap(const MemoryMap &) = delete;
+    auto operator=(MemoryMap &&o) -> MemoryMap &;
+    auto operator=(const MemoryMap &) -> MemoryMap & = delete;
+    ~MemoryMap();
+
+    [[nodiscard]] auto desc_count() const -> usize { return m_desc_count; }
+    [[nodiscard]] auto desc_size() const -> usize { return m_desc_size; }
+    [[nodiscard]] auto desc_version() const -> u32 { return m_desc_version; }
+    [[nodiscard]] auto at(usize i) const -> MemoryDescriptor * {
+      const auto *u8_ptr = bit_cast<u8 *>(m_mmap);
+      const auto offset = i * m_desc_size;
+      assert_invariant(offset < desc_count() * desc_size());
+
+      return bit_cast<MemoryDescriptor *>(u8_ptr + offset);
+    }
+
+    [[nodiscard]] static auto current() -> Nullable<MemoryMap>;
+
+    void dump(auto &os) const {
+      auto ss = os << u"Memory Map Dump:" << ENDL;
+      for (usize i = 0; i < desc_count(); i++) {
+        const auto *desc = at(i);
+
+        ss << u"          Desc " << i << u": Ty: " << desc->type() << u", PhysAddr: " << desc->physical_start()
+           << u", N: " << desc->number_of_pages() << ENDL;
+      }
+    }
+  };
+}  // namespace wesos::boot::efi
---
+++ src/boot/src/MemoryMap.cc
@@ -0,0 +1,159 @@
+/**
+ * This file is part of the WesOS project.
+ *
+ * WesOS is public domain software: you can redistribute it and/or modify
+ * it under the terms of the Unlicense(https://unlicense.org/).
+ */
+
+#include <boot/Logger.hh>
+#include <boot/MemoryMap.hh>
+
+using namespace wesos;
+using namespace wesos::boot::efi;
+
+auto MemoryMap::initialize_memory_map_details(MemoryMap &mmap) -> bool {
+  usize mmap_size = 0;
+  usize map_key = 0;
+  usize desc_size = 0;
+  u32 desc_ver = 0;
+
+  NOTE << u"Requesting the size of the memory map..." << ENDL;
+
+  const auto mmap_api = SYSTEM_TABLE_GLOBAL->boot_services().m_get_memory_map;
+  const auto status = mmap_api(&mmap_size, nullptr, &map_key, &desc_size, &desc_ver);
+
+  if (status != EFI_BUFFER_TOO_SMALL && status != EFI_SUCCESS) [[unlikely]] {
+    FAIL << u"Unable to determine the size of the UEFI memory map." << ENDL;
+    return false;
+  }
+
+  NOTE << u"The minimum buffer size needed to hold the UEFI memory map is " << mmap_size << u" bytes." << ENDL;
+
+  { /**
+     * Double the memory map size to minimize the chances
+     * that the reported minimum buffer size becomes inadequate
+     * due to changes in the memory map (by firmware threads?).
+     */
+
+    mmap_size *= 2;
+    NOTE << u"Doubled the UEFI memory map buffer size from " << mmap_size / 2 << u" bytes to " << mmap_size
+         << u" bytes." << ENDL;
+  }
+
+  INFO << u"The UEFI memory map buffer size is " << mmap_size << u" bytes." << ENDL;
+
+  mmap.m_mmap = nullptr;
+  mmap.m_mmap_size = mmap_size;
+  mmap.m_desc_count = 0;
+  mmap.m_desc_size = desc_size;
+  mmap.m_desc_version = desc_ver;
+
+  return true;
+}
+
+auto MemoryMap::create_memory_map_buffer(MemoryMap &mmap) -> bool {
+  const auto allocate_pool_api = SYSTEM_TABLE_GLOBAL->boot_services().m_allocate_pool;
+
+  NOTE << u"Allocating " << mmap.m_mmap_size << u" bytes for the UEFI memory map..." << ENDL;
+
+  void *desc_array = nullptr;
+  if (allocate_pool_api(MemoryType::LoaderData, mmap.m_mmap_size, &desc_array) != EFI_SUCCESS) {
+    FAIL << u"Unable to allocate " << mmap.m_mmap_size << u" bytes for the UEFI memory map." << ENDL;
+    return false;
+  }
+
+  INFO << u"Allocated " << mmap.m_mmap_size << u" bytes for the UEFI memory map." << ENDL;
+
+  mmap.m_mmap = static_cast<MemoryDescriptor *>(desc_array);
+
+  return true;
+}
+
+auto MemoryMap::retrieve_memory_map(MemoryMap &mmap) -> bool {
+  NOTE << u"Retrieving the UEFI memory map..." << ENDL;
+
+  usize map_key = 0;
+  const auto mmap_api = SYSTEM_TABLE_GLOBAL->boot_services().m_get_memory_map;
+  const auto status = mmap_api(&mmap.m_mmap_size, mmap.m_mmap, &map_key, &mmap.m_desc_size, &mmap.m_desc_version);
+
+  if (status != EFI_SUCCESS) {
+    FAIL << u"Failed to retrive the UEFI memory map." << ENDL;
+    return false;
+  }
+
+  mmap.m_desc_count = mmap.m_mmap_size / mmap.m_desc_size;
+
+  NOTE << u"UEFI memory map descriptor size: " << mmap.m_desc_size << u" bytes." << ENDL;
+  NOTE << u"UEFI memory map descriptor count: " << mmap.m_desc_count << u"." << ENDL;
+  NOTE << u"UEFI memory map descriptor version: " << mmap.m_desc_version << u"." << ENDL;
+
+  INFO << u"Successfully retrieved the UEFI memory map." << ENDL;
+
+  return true;
+}
+
+MemoryMap::MemoryMap(MemoryMap &&o)
+    : m_mmap(o.m_mmap), m_desc_count(o.m_desc_count), m_desc_size(o.m_desc_size), m_desc_version(o.m_desc_version) {
+  o.m_mmap = nullptr;
+  o.m_desc_count = 0;
+  o.m_desc_size = 0;
+  o.m_desc_version = 0;
+}
+
+auto MemoryMap::operator=(MemoryMap &&o) -> MemoryMap & {
+  if (this != &o) [[likely]] {
+    m_mmap = o.m_mmap;
+    m_desc_count = o.m_desc_count;
+    m_desc_size = o.m_desc_size;
+    m_desc_version = o.m_desc_version;
+
+    o.m_mmap = nullptr;
+    o.m_desc_count = 0;
+    o.m_desc_size = 0;
+    o.m_desc_version = 0;
+  }
+
+  return *this;
+}
+
+MemoryMap::~MemoryMap() {
+  if (m_mmap != nullptr) {
+    SYSTEM_TABLE_GLOBAL->boot_services().m_free_pool(m_mmap);
+  }
+}
+
+auto MemoryMap::current() -> Nullable<MemoryMap> {
+  const auto max_failure_count = 10;
+
+  usize failure_count = 0;
+
+  while (failure_count < max_failure_count) {
+    NOTE << u"Requesting the system memory map..." << ENDL;
+
+    MemoryMap mmap;
+
+    if (!initialize_memory_map_details(mmap)) [[unlikely]] {
+      FAIL << u"Failed to get the buffer size for the memory map." << ENDL;
+      failure_count++;
+      continue;
+    }
+
+    if (!create_memory_map_buffer(mmap)) [[unlikely]] {
+      FAIL << u"Failed to allocate memory for the memory map." << ENDL;
+      failure_count++;
+      continue;
+    }
+
+    if (!retrieve_memory_map(mmap)) [[unlikely]] {
+      FAIL << u"Failed to populate the memory map." << ENDL;
+      failure_count++;
+      continue;
+    }
+
+    return mmap;
+  }
+
+  FAIL << u"Unable to get the UEFI memory map after " << failure_count << u" attempts." << ENDL;
+
+  return nullptr;
+}
